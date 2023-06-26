83dfd344ab321dd4767eba7a54c1a1b62f8e2bb4
JulianIrigoyen <julian.irigoyen@experoinc.com>
2023-06-26 20:48:41 +0200

    Oscillator now reproduces list of notes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,132 @@
-use core::time::Duration;
-use rodio::{OutputStream, source::Source};
+use rodio::{OutputStream, OutputStreamHandle, source::Source};
+use std::collections::HashMap;
+use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
 
-fn main() {
+/*
+      We want to write a wavetable oscillator: an object that iterates over a specific wave table
+      with speed dictated by the frequency of the tone it should output.
+      That object needs to store the sampling rate, the wave table, current index into the wave table,
+      and the frequency-dependent index increment.
+   */
 
+struct WavetableOscillator {
+    sample_rate: u32,
+    wave_table: Vec<f32>,
+    index: f32,
+    index_increment: f32,
+}
+
+impl WavetableOscillator {
+    fn new(sample_rate: u32, wave_table: Vec<f32>) -> WavetableOscillator {
+        return WavetableOscillator {
+            sample_rate: sample_rate,
+            wave_table: wave_table,
+            index: 0.0,
+            index_increment: 0.0,
+        };
+    }
+
+    /*
+        Sets the frequency of the wavetable oscillator by calculating the index_increment value.
+        The index_increment determines how quickly the oscillator moves through the wavetable
+        to generate the waveform.
+
+        Setting the frequency is essential because it determines the pitch of the sound produced by the wavetable oscillator.
+         A higher frequency will result in a higher-pitched sound, while a lower frequency will produce a lower-pitched sound. By adjusting the frequency dynamically, we can generate different musical notes and create melodies
+
+        The set_frequency function allows us to conveniently update the frequency parameter
+        of the oscillator and adjust its output in real-time.
+     */
+    fn set_frequency(&mut self, frequency: f32) {
+        self.index_increment = frequency * self.wave_table.len() as f32
+            / self.sample_rate as f32;
+    }
+
+    /*
+        Generating a sample consists of linear interpolation of the wave table values according to the index value and incrementing the index.
+     */
+
+    fn get_sample(&mut self) -> f32 {
+        let sample = self.lerp();
+        self.index += self.index_increment;
+        self.index %= self.wave_table.len() as f32;
+        return sample;
+    }
+
+    fn lerp(&self) -> f32 {
+        let truncated_index = self.index as usize;
+        let next_index = (truncated_index + 1) % self.wave_table.len();
+
+        let next_index_weight = self.index - truncated_index as f32;
+        let truncated_index_weight = 1.0 - next_index_weight;
+
+        return truncated_index_weight * self.wave_table[truncated_index]
+            + next_index_weight * self.wave_table[next_index];
+    }
+}
+
+impl Iterator for WavetableOscillator {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        return Some(self.get_sample());
+    }
+}
+
+impl Source for WavetableOscillator {
+    fn channels(&self) -> u16 {
+        return 1;
+    }
+
+    fn sample_rate(&self) -> u32 {
+        return self.sample_rate;
+    }
+
+    fn current_frame_len(&self) -> Option<usize> {
+        return None;
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        return None;
+    }
+}
+
+
+fn create_note_to_freq_map() -> HashMap<String, f32> {
+    let mut map = HashMap::new();
+    map.insert("A".to_string(), 440.00);
+    map.insert("A#".to_string(), 466.16);
+    map.insert("B".to_string(), 493.88);
+    map.insert("C".to_string(), 523.25);
+    map.insert("C#".to_string(), 554.37);
+    map.insert("D".to_string(), 587.33);
+    map.insert("D#".to_string(), 622.25);
+    map.insert("E".to_string(), 659.25);
+    map.insert("F".to_string(), 698.46);
+    map.insert("F#".to_string(), 739.99);
+    map.insert("G".to_string(), 783.99);
+    map.insert("G#".to_string(), 830.61);
+
+    map
+}
+
+fn play_notes(notes: Vec<&str>, duration: f32, stream_handle: &OutputStreamHandle, wave_table: Vec<f32>) {
+    let note_to_freq_map = create_note_to_freq_map();
+    for note in notes {
+        // set the frequency
+        let frequency = note_to_freq_map.get(note).cloned().unwrap_or(440.0);  //
+        let mut oscillator = WavetableOscillator::new(44100, wave_table.clone());
+        oscillator.set_frequency(frequency);
+        stream_handle.play_raw(oscillator.convert_samples());
+        // sleep for the duration
+        std::thread::sleep(std::time::Duration::from_secs_f32(duration));
+    }
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
     //A wave table is an array in memory, which contains 1 period of the waveform
     // we want to play out through our oscillator.
     let wave_table_size = 64;
@@ -20,101 +144,24 @@ fn main() {
         wave_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
     }
 
-    let mut oscillator = WavetableOscillator::new(44100, wave_table);
-    oscillator.set_frequency(440.0);
+    // let mut oscillator = WavetableOscillator::new(44100, wave_table);
+    // oscillator.set_frequency(440.0);
+    //
+    // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    //
+    // let _result = stream_handle.play_raw(oscillator.convert_samples());
+    //
+    // std::thread::sleep(std::time::Duration::from_secs(5));
 
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // define a sequence of notes to play
+    let notes = vec!["A", "B", "C", "D", "E", "F", "G", "A"];
+    // duration for each note in seconds
+    let duration = 0.5;
 
-    let _result = stream_handle.play_raw(oscillator.convert_samples());
+    let (_stream, stream_handle) = OutputStream::try_default()?;
 
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    // Call the function with the note sequence, duration, stream_handle, and wave_table
+    play_notes(notes, duration, &stream_handle, wave_table);
 
-    /*
-        We want to write a wavetable oscillator: an object that iterates over a specific wave table
-        with speed dictated by the frequency of the tone it should output.
-        That object needs to store the sampling rate, the wave table, current index into the wave table,
-        and the frequency-dependent index increment.
-     */
-
-    struct WavetableOscillator {
-        sample_rate: u32,
-        wave_table: Vec<f32>,
-        index: f32,
-        index_increment: f32,
-    }
-
-    impl WavetableOscillator {
-        fn new(sample_rate: u32, wave_table: Vec<f32>) -> WavetableOscillator {
-            return WavetableOscillator {
-                sample_rate: sample_rate,
-                wave_table: wave_table,
-                index: 0.0,
-                index_increment: 0.0,
-            };
-        }
-
-        /*
-            Sets the frequency of the wavetable oscillator by calculating the index_increment value.
-            The index_increment determines how quickly the oscillator moves through the wavetable
-            to generate the waveform.
-
-            Setting the frequency is essential because it determines the pitch of the sound produced by the wavetable oscillator.
-             A higher frequency will result in a higher-pitched sound, while a lower frequency will produce a lower-pitched sound. By adjusting the frequency dynamically, we can generate different musical notes and create melodies
-
-            The set_frequency function allows us to conveniently update the frequency parameter
-            of the oscillator and adjust its output in real-time.
-         */
-        fn set_frequency(&mut self, frequency: f32) {
-            self.index_increment = frequency * self.wave_table.len() as f32
-                / self.sample_rate as f32;
-        }
-
-        /*
-            Generating a sample consists of linear interpolation of the wave table values according to the index value and incrementing the index.
-         */
-
-        fn get_sample(&mut self) -> f32 {
-            let sample = self.lerp();
-            self.index += self.index_increment;
-            self.index %= self.wave_table.len() as f32;
-            return sample;
-        }
-
-        fn lerp(&self) -> f32 {
-            let truncated_index = self.index as usize;
-            let next_index = (truncated_index + 1) % self.wave_table.len();
-
-            let next_index_weight = self.index - truncated_index as f32;
-            let truncated_index_weight = 1.0 - next_index_weight;
-
-            return truncated_index_weight * self.wave_table[truncated_index]
-                + next_index_weight * self.wave_table[next_index];
-        }
-    }
-
-    impl Iterator for WavetableOscillator {
-        type Item = f32;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            return Some(self.get_sample());
-        }
-    }
-
-    impl Source for WavetableOscillator {
-        fn channels(&self) -> u16 {
-            return 1;
-        }
-
-        fn sample_rate(&self) -> u32 {
-            return self.sample_rate;
-        }
-
-        fn current_frame_len(&self) -> Option<usize> {
-            return None;
-        }
-
-        fn total_duration(&self) -> Option<Duration> {
-            return None;
-        }
-    }
+    Ok(())
 }
